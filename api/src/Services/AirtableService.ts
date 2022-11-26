23ee5c946c12d1ab34ed2008a794e31c99880d5b
Ike Nwosu <ike.nwosu.151@gmail.com>
2022-11-25 22:37:56 -0500

    Adding filter builder

--- api/src/Services/AirtableService.ts
+++ api/src/Services/AirtableService.ts
@@ -1,4 +1,6 @@
 import  airtable from 'airtable'
+import { FieldSet } from 'airtable/lib/field_set';
+import { QueryParams } from 'airtable/lib/query_params';
 
 
 export default class AirtableService{
@@ -11,14 +13,28 @@ export default class AirtableService{
     async getRecordById(table, recordId){
         console.log(`Getting record ${recordId}`)
         console.log(`Getting from table ${table}`)
-        return await this._base('Events').find(recordId)
+        return await this._base(table).find(recordId)
     }
 
     getRecordsList(tableName, take ){
-
     }
 
-    getRecordByMatch(table: string, match:{}){
+    async getRecordsByFields(table: string, fields:{}){
+        return await this._base(table).select().all(fields)
+    }
 
+    filterByFormulaBuilder(match:{}){
+        let formula = ""
+        let keys = Object.keys(match)
+        let values = Object.values(match)
+        if(keys.length == 0) return
+
+        keys.forEach((key, i)=>{
+            if(formula.length > 0){
+                formula += ', '
+            }
+            formula += `{key} = "${values[i]}` 
+        })
+        return `AND (${formula})`
     }
 }
