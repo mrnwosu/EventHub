4d2c8228c27110a1d706e145de849723b20b59a5
Ike Nwosu <ike.nwosu.151@gmail.com>
2022-12-04 22:55:44 -0500

    Air Service Updates

--- scripts/air.py
+++ scripts/air.py
@@ -1,43 +1,36 @@
 from pyairtable import Api, Base, Table
 from pyairtable.formulas import match
+import json
 
 class AirtableService:
     def __init__(self, api_key: str, base_id: str):
       self.api_key = api_key
       self.base_id = base_id
     
+    def get_table(self, table_name: str) -> Table: 
+        return Table(self.api_key, self.base_id, table_name)
 
-    def set_table(self, table_name: str) -> Table: 
-        print("Setting Table Here")
-        print(f'API KEY => {self.api_key}')
-        print(f'BASE ID => {self.base_id}')
-        self.table = Table(self.api_key, self.base_id, table_name)
+    def get_all_records(self, table: str) -> list[dict]:
+        return self.get_table(table).all()
 
-    # Gets 
-    def get_all_records(self) -> list[dict]:
-        return self.table.all()
+    def get_record_by_id(self, table:str, record_id: str) -> dict:
+        return self.get_table(table).get(record_id)
 
-    def get_record_by_id(self, record_id: str) -> dict:
-        return self.table.get(record_id)
-
-    def get_records_by_name(self, record_name: str) -> list[dict]:
-        return self.get_records_by_match({'name': record_name})
-
-    def get_records_by_match(self, match_dict: dict) -> list[dict]:
-        return self.table.all(formula=match(match_dict))
+    def get_records_by_match(self, table, match_dict: dict) -> list[dict]:
+        return self.get_table(table).all(formula=match(match_dict))
 
     # Deletes
-    def delete_by_id(self, record_id: str):
-        return self.table.delete(record_id)
+    def delete_by_id(self, table: str, record_id: str):
+        return self.get_table(table).delete(record_id)
 
-    def delete_by_match(self, match: dict):
-        records = self.get_records_by_match(match)
+    def delete_by_match(self, table: str, match: dict):
+        records = self.get_records_by_match(table, match)
         ids = list(map(lambda r: r['id'], records))
-        return self.table.batch_delete(ids)
+        return self.get_table(table).batch_delete(ids)
         
     # Creates   
-    def create_record(self, fields: dict):
-        return self.table.create(fields)
+    def create_record(self, table: str, fields: dict):
+        return self.get_table(table).create(fields)
     
-    def update_record(self, record_id: str,  fields_to_update: dict, replace = False):
-        return self.table.update(record_id,fields_to_update, replace)
+    def update_record(self, table: str, record_id: str,  fields_to_update: dict, replace = False):
+        return self.get_table(table).update(record_id,fields_to_update, replace)
