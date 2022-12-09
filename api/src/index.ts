ff29d6ab58de8ad5267c13d0ca81e57cd484cc4e
Ike Nwosu <ike.nwosu.151@gmail.com>
2022-12-08 22:34:43 -0500

    IN - Updating .gitignore

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,6 @@
 import ApiController from './Services/ApiController'
 
 //Get someone configuration
-const port = 8000
-
+const port = process.env['EVENTHUB_PORT'] ?? "80"
 const controller = new ApiController(port)
 controller.run()
